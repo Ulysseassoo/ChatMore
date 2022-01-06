176fdc822b1b7f4d55ccc124a0e79bd9bf26acef
Ulysse <ulysse.asso-o-emane@supinternet.fr>
2022-01-07 00:09:54 +0100

    Add main Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
-function App() {
-	return <h1>Hello World</h1>
+import React from "react"
+import { Route, Routes } from "react-router"
+import { ThemeProvider } from "styled-components"
+import Homepage from "./Screens/Homepage"
+import Login from "./Screens/Login"
+import Register from "./Screens/Register"
+import { GlobalStyles } from "./Theme/global"
+import { lightTheme } from "./Theme/theme"
+
+const App: React.FC = () => {
+	return (
+		<ThemeProvider theme={lightTheme}>
+			<GlobalStyles />
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+			</Routes>
+		</ThemeProvider>
+	)
 }
 
 export default App
---
+++ src/Screens/Register.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Register: React.FC = () => {
+	return <div>Register</div>
+}
+
+export default Register
