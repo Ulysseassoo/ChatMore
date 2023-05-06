713ca386b68c57d5411b3f4535abfe9c6d4d578c
Ulysse <ulysse.asso-o-emane@supinternet.fr>
2023-05-06 14:44:37 +0200

    Added analytics for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "@fontsource/heebo";
 import "@fontsource/convergence";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<App />
+		<Analytics />
 	</React.StrictMode>,
 );
